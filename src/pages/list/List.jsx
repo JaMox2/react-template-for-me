4ce11203b824681d1f95f4a8ddb00b4842f391fe
JaMox2 <jeffreyvillamilwork@gmail.com>
2022-07-29 13:34:22 +0800

    done

--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -5,6 +5,7 @@ import { useLocation } from 'react-router-dom'
 import { useState } from "react"
 import { format } from "date-fns"
 import { DateRange } from "react-date-range"
+import SearchItem from "../../components/searchitem/SearchItem"
 
 export default function List() {
 
@@ -61,7 +62,16 @@ export default function List() {
             </div>
             <button>Search</button>
           </div>
-          <div className="listResult"></div>
+          <div className="listResult">
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+            <SearchItem/>
+          </div>
         </div>
       </div>
     </div>
